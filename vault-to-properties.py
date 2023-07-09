1b29131a06231fa47519e14ec9175545a33ef950
Mohammad Baghban <mohamad.baghban@snapp.cab>
2023-07-09 20:13:32 +0330

    feat: add support for properties without "vault" prefix

--- vault-to-properties.py
+++ vault-to-properties.py
@@ -5,9 +5,9 @@ import sys
 
 url = sys.argv[1]
 application_properties_address = sys.argv[2]
+vault_token = sys.argv[3]
+vault_version = sys.argv[4]
 
-vault_token = str(input("Enter vault token\n"))
-vault_version = str(input("Enter vault version\n"))
 querystring = {"version": vault_version}
 payload = ""
 headers = {"X-Vault-Token": vault_token}
@@ -20,11 +20,15 @@ with open(application_properties_address, "r+") as application_properties:
     text = application_properties.read()
     file_str = text
     for property_name in properties:
+        new_value = properties[property_name]
+        print(property_name + " = " + new_value)
         if "vault." in property_name:
-            new_value = properties[property_name]
             pattern = fr"({re.escape(property_name)}:)(\w+)"
             file_str = re.sub(pattern, fr"\g<1>{new_value}", file_str)
+        else:
+            pattern = fr"({re.escape(property_name)}=).*"
+            file_str = re.sub(pattern, fr"\g<1>{new_value}", file_str)
 
-    print(file_str)
+    # print(file_str)
     with open("application.properties", "w+") as file2:
         file2.write(file_str)
