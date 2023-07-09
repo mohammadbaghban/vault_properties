628433ba154d5efb8926ad62e8b0112bba5b888d
Mohammad Baghban <mohamad.baghban@snapp.cab>
2023-07-09 10:40:57 +0330

    feat: initial commit

---
+++ vault-to-properties.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import re
+import sys
+
+url = sys.argv[1]
+application_properties_address = sys.argv[2]
+
+vault_token = str(input("Enter vault token\n"))
+vault_version = str(input("Enter vault version\n"))
+querystring = {"version": vault_version}
+payload = ""
+headers = {"X-Vault-Token": vault_token}
+
+response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+data = json.loads(response.text)
+properties = data["data"]["data"]
+
+with open(application_properties_address, "r+") as application_properties:
+    text = application_properties.read()
+    file_str = text
+    for property_name in properties:
+        if "vault." in property_name:
+            new_value = properties[property_name]
+            pattern = fr"({re.escape(property_name)}:)(\w+)"
+            file_str = re.sub(pattern, fr"\g<1>{new_value}", file_str)
+
+    print(file_str)
+    with open("application.properties", "w+") as file2:
+        file2.write(file_str)
